e23c5480cd5821d18add48026aa12bb88e7cf369
Jibin <39941719+jokercsi@users.noreply.github.com>
2022-02-23 12:22:53 +0900

    Create 숫자 문자열과 영단어.py

---
+++ python/숫자 문자열과 영단어.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = 0
+    
+    dic = {"zero": "0","one": "1", "two": "2", "three":"3", "four": "4","five":"5", "six" :"6", "seven" : "7", "eight" : "8", "nine":"9"}
+
+    for i, j in dic.items():
+        s = s.replace(i, j)
+    answer = s
+    
+    return int(answer)
